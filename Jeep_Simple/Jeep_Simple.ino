16321e5334679a15a1c058ff134a0335b56cb896
Brent Brooks <brent@brentnet.com>
2020-01-20 17:24:03 -0500

    Fixed issue #1

--- Jeep_Simple/Jeep_Simple.ino
+++ Jeep_Simple/Jeep_Simple.ino
@@ -1,8 +1,8 @@
 #include "JeepLights.h"
 FogMM FogFL(0,FOGFL_PIN,6);
 FogMM FogFR(1,FOGFR_PIN,6);
-Spot SpotL(2,SPOTL_PIN,2);
-Spot SpotR(3,SPOTR_PIN,2);
+Spot SpotL(2,SPOTL_PIN,1);
+Spot SpotR(3,SPOTR_PIN,1);
 #include "Next-Info.h"
 
 void setup() 
